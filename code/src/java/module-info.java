c045186c1e9b8d2ec15562840cfcd35b82af114a
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2021-11-24 18:34:05 +0100

    Changed the structure of everything lol

--- code/src/java/module-info.java
+++ code/src/java/module-info.java
@@ -2,6 +2,6 @@ module com.futuremctests {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.futuremctests to javafx.fxml;
-    exports com.futuremctests;
+    opens futuremctests to javafx.fxml;
+    exports futuremctests;
         }
