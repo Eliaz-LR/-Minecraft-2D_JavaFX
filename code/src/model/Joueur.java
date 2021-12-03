60fc67385fc658f4d551c89411d32a011db679b1
Ben <bengregorygbr@hotmail.com>
2021-12-03 18:36:51 +0100

    ajout d'attribut pour faire changer le joueur de direction + binding PlayerView

--- code/src/model/Joueur.java
+++ code/src/model/Joueur.java
@@ -1,12 +1,29 @@
 package model;
 
 import java.awt.*;
+
+import com.sun.webkit.dom.DocumentImpl;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.transform.Rotate;
 
 public class Joueur {
+    //image de base
     public Image img = new Image("/images/steve.png", 160/2, 360/2, true, true);
+    //image pour aller de chaque coté
+    public Image imgD = new Image("/images/steve-right.png", 160/2, 360/2, true, true);
+    public Image imgG = new Image("/images/steve.png", 160/2, 360/2, true, true);
+
     //les coordonnées du joueur sont ici données en "unités" de canvas
-    public float x;
+    public double x;
     public float y;
+
+    public DoubleProperty xP = new SimpleDoubleProperty(x);
+    public DoubleProperty yP = new SimpleDoubleProperty(y);
+
+
+
 
 }
