903691991171421ee5ed86c203b5dc61e8daf3f6
Ben <bengregorygbr@hotmail.com>
2021-12-09 15:45:19 +0100

    ajout méthode checkBlocks

--- code/src/launch/Launch.java
+++ code/src/launch/Launch.java
@@ -2,23 +2,17 @@ package launch;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.input.KeyEvent;
 import model.*;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 import model.Coordonnees;
-import view.PlayerView;
 
 public class Launch extends Application {
 
@@ -34,28 +28,28 @@ public class Launch extends Application {
         Scene mainJeu = new Scene(root);
         primaryStage.setScene(mainJeu);
 
-        Canvas canvas = new Canvas(1000, 800);
+        Canvas canvas = new Canvas(1200, 900);
         root.getChildren().add(canvas);
 
         double widthSteve = 40;
         double heightSteve = 90;
-        Joueur joueur = new Joueur(widthSteve, heightSteve);
         int blockSize = 40;
-
-        //PlayerView plv = new PlayerView(joueur);
-        //deplacerJoueur deplacerPlv = new deplacerJoueur(joueur, mainJeu, plv);
-
+        Joueur joueur = new Joueur(widthSteve, heightSteve);
         deplacerJoueur deplacerJoueur = new deplacerJoueur(joueur, mainJeu);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
 
-
-        gc.setFill(Color.LIGHTBLUE);
-        gc.setStroke(Color.DARKGRAY);
-        drawGrid grid = new drawGrid(blockSize);
+        Mouse mouse = new Mouse(mainJeu);
         Coordonnees coo = new Coordonnees();
         coo.x = canvas.getWidth()/2;
         coo.y = canvas.getHeight()/2;
-        Mouse mouse = new Mouse(mainJeu);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.LIGHTBLUE);
+        gc.setStroke(Color.DARKGRAY);
+
+        drawGrid grid = new drawGrid(blockSize);
+
+
+
 
 
         final long startNanoTime = System.nanoTime();
@@ -67,12 +61,9 @@ public class Launch extends Application {
                 //converti et affiche les positions du joueur depuis le canvas vers le monde, les coordonées du joueur sont faites depuis son milieu.
                 Coordonnees coo_joueur_dans_monde = coo.CanvasToPosition(canvas.getWidth()/2,canvas.getHeight()/2, joueur.x, joueur.y,canvas, grid.cellSize);
                 //System.out.println(coo_joueur_dans_monde.x+" "+coo_joueur_dans_monde.y+" id: "+grid.monde.getType((int)coo_joueur_dans_monde.x,(int)coo_joueur_dans_monde.y).toString());
-                if (grid.monde.getType((int)coo_joueur_dans_monde.x,1+(int)coo_joueur_dans_monde.y).toString().equals("Air")){
-                    deplacerJoueur.IsBlockDownEmpty = true;
-                }
-                else {
-                    deplacerJoueur.IsBlockDownEmpty = false;
-                }
+
+
+                checkBlocks(grid, deplacerJoueur, coo_joueur_dans_monde);
                 deplacerJoueur.deplacerJoueur();
                 if (mouse.coordSet()){
                     Coordonnees coord_mouse = coo.CanvasToPosition(mouse.x, mouse.y, joueur.x, joueur.y,canvas, grid.cellSize);
@@ -82,11 +73,48 @@ public class Launch extends Application {
                 gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
                 grid.drawMonde(canvas, joueur, canvas.getWidth(), canvas.getHeight());
                 gc.drawImage(joueur.img, canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightSteve/2);
-                //gc.drawImage(plv.img, canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightSteve/2);
             }
         }.start();
 
         primaryStage.show();
+
+
+
+    }
+
+    public void checkBlocks(drawGrid grid, deplacerJoueur deplacerJoueur, Coordonnees coo_joueur_dans_monde){
+        //verification pour les blocs en DESSOUS
+        if (grid.monde.getType((int)coo_joueur_dans_monde.x,1+(int)coo_joueur_dans_monde.y).toString().equals("Air")){
+            deplacerJoueur.IsBlockDownEmpty = true;
+        }
+        else {
+            deplacerJoueur.IsBlockDownEmpty = false;
+        }
+
+        //vérification pour les blocs au DESSUS
+        if (grid.monde.getType((int)coo_joueur_dans_monde.x,(int)coo_joueur_dans_monde.y - 1).toString().equals("Air")){
+            deplacerJoueur.IsBlockUpEmpty = true;
+        }
+        else {
+            deplacerJoueur.IsBlockUpEmpty= false;
+        }
+
+        //vérification pour les blocs à GAUCHE
+        if(grid.monde.getType((int)coo_joueur_dans_monde.x -1, (int)coo_joueur_dans_monde.y).toString().equals("Air")){
+            deplacerJoueur.IsBlockLeftEmpty = true;
+        }
+        else{
+            deplacerJoueur.IsBlockLeftEmpty = false;
+        }
+
+        //vérification pour les blocs a DROITE
+        if(grid.monde.getType((int)coo_joueur_dans_monde.x+1, (int)coo_joueur_dans_monde.y).toString().equals("Air")){
+            deplacerJoueur.IsBlockRightEmpty = true;
+        }
+        else{
+            deplacerJoueur.IsBlockRightEmpty = false;
+        }
+
     }
 
 }
