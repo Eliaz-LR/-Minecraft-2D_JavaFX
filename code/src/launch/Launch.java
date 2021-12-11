3bac61ce07c548b0c0f30ea12c8f518e4f1869e0
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2021-12-11 09:54:38 +0100

    unsuccessfull attempts at making a working main menu

--- code/src/launch/Launch.java
+++ code/src/launch/Launch.java
@@ -2,6 +2,7 @@ package launch;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -10,25 +11,34 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.*;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import model.Coordonnees;
 
 public class Launch extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws FileNotFoundException {
+    public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Minecraft 2D");
-
+        primaryStage.setResizable(false);
+        /*
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainMenu.fxml"));
+        Scene mainMenu = new Scene(loader.load());
+        primaryStage.setScene(mainMenu);
+        primaryStage.show();
+        */
+        startGame(primaryStage);
+    }
+    public void startGame(Stage primaryStage){
         Group root = new Group();
         Scene mainJeu = new Scene(root);
+
         primaryStage.setScene(mainJeu);
 
-        Canvas canvas = new Canvas(1200, 900);
+        Canvas canvas = new Canvas(1200, 800);
         root.getChildren().add(canvas);
 
         double widthSteve = 40;
@@ -48,10 +58,6 @@ public class Launch extends Application {
         gc.setStroke(Color.RED);
 
         drawGrid grid = new drawGrid(blockSize);
-
-
-
-
 
         final long startNanoTime = System.nanoTime();
         //d'apres le prof, plutot utiliser un thread pour la boucle
@@ -74,6 +80,61 @@ public class Launch extends Application {
                 gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
                 grid.drawMonde(canvas, joueur, canvas.getWidth(), canvas.getHeight());
                 gc.drawImage(joueur.img, canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightSteve/2);
+
+                gc.strokeRect(canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightHitbox/2, widthSteve, heightHitbox);
+            }
+        }.start();
+
+        primaryStage.show();
+    }
+/*
+        Scene mainJeu = new Scene(root);
+
+        primaryStage.setScene(mainJeu);
+
+        Canvas canvas = new Canvas(1200, 800);
+        root.getChildren().add(canvas);
+
+        double widthSteve = 40;
+        double heightSteve = 90;
+        double heightHitbox = heightSteve - 10;
+        int blockSize = 40;
+        Joueur joueur = new Joueur(widthSteve, heightSteve);
+        deplacerJoueur deplacerJoueur = new deplacerJoueur(joueur, mainJeu);
+
+        Mouse mouse = new Mouse(mainJeu);
+        Coordonnees coo = new Coordonnees();
+        coo.x = canvas.getWidth()/2;
+        coo.y = canvas.getHeight()/2;
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.LIGHTBLUE);
+        gc.setStroke(Color.RED);
+
+        drawGrid grid = new drawGrid(blockSize);
+
+        final long startNanoTime = System.nanoTime();
+        //d'apres le prof, plutot utiliser un thread pour la boucle
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+
+                //System.out.println(deplacerJoueur.input); //affiche la touche dans le terminal
+                //converti et affiche les positions du joueur depuis le canvas vers le monde, les coordonées du joueur sont faites depuis son milieu.
+                Coordonnees coo_joueur_dans_monde = coo.CanvasToPosition(canvas.getWidth()/2,canvas.getHeight()/2, joueur.x, joueur.y,canvas, grid.cellSize);
+                System.out.println(coo_joueur_dans_monde.x+" "+coo_joueur_dans_monde.y+" id: "+grid.monde.getType((int)coo_joueur_dans_monde.x,(int)coo_joueur_dans_monde.y).toString());
+
+
+                checkBlocks(grid, deplacerJoueur, coo_joueur_dans_monde);
+                deplacerJoueur.deplacerJoueur();
+                if (mouse.coordSet()){
+                    Coordonnees coord_mouse = coo.CanvasToPosition(mouse.x, mouse.y, joueur.x, joueur.y,canvas, grid.cellSize);
+                    grid.monde.setType((int)coord_mouse.x,(int)coord_mouse.y,new Type(EnumType.Air));
+                    mouse.resetCoord();
+                }
+                gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                grid.drawMonde(canvas, joueur, canvas.getWidth(), canvas.getHeight());
+                gc.drawImage(joueur.img, canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightSteve/2);
+
                 gc.strokeRect(canvas.getWidth()/2-widthSteve/2, canvas.getHeight()/2-heightHitbox/2, widthSteve, heightHitbox);
             }
         }.start();
@@ -84,6 +145,8 @@ public class Launch extends Application {
 
     }
 
+
+*/
     public void checkBlocks(drawGrid grid, deplacerJoueur deplacerJoueur, Coordonnees coo_joueur_dans_monde){
         //verification pour les blocs en DESSOUS
         if (grid.monde.getType((int)coo_joueur_dans_monde.x,1+(int)coo_joueur_dans_monde.y).toString().equals("Air")){
