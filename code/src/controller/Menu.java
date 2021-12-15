3e8b37693617fca4be4ef7fab7a0ee7311da072c
Ben <bengregorygbr@hotmail.com>
2021-12-15 23:15:53 +0100

    modification Launch

---
+++ code/src/controller/Menu.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import model.GameManager;
+
+public class Menu {
+    @FXML
+    private Button buttonPlay;
+
+    @FXML private javafx.scene.control.Button buttonClose;
+
+     //buttonPlay.setOnAction(event -> startGame(primaryStage)); fonctionnera quand il aura le singleton
+
+    public void handleClose (ActionEvent actionEvent){
+        // get a handle to the stage
+        Stage stage = (Stage) buttonClose.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
+
+    public void handlePlay(ActionEvent actionEvent) {
+        GameManager.getInstance().startGame();
+
+    }
+}
