75bd39bc64d73f780b97317d38b4ee38c13e5c2e
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-01-07 18:32:16 +0100

    Update Generation.java

--- code/src/controller/Generation.java
+++ code/src/controller/Generation.java
@@ -6,10 +6,10 @@ import javafx.stage.Stage;
 import model.GameManager;
 
 public class Generation {
-    @FXML private javafx.scene.control.Button btn_generate;
+    @FXML private javafx.scene.control.Button valider_gen;
 
     public void handleGenerate(ActionEvent actionEvent) {
-        Stage stage = (Stage) btn_generate.getScene().getWindow();
+        Stage stage = (Stage) valider_gen.getScene().getWindow();
         stage.close();
         GameManager.getInstance().startGame(80,30);
     }
