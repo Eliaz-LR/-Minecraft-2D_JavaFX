96ba489cd821b0a8c5573e4eeb0488119c66f12f
Ben <bengregorygbr@hotmail.com>
2022-01-07 19:20:01 +0100

    début modif cycle jour nuit

--- code/src/model/Cycle.java
+++ code/src/model/Cycle.java
@@ -3,8 +3,22 @@ package model;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 import javafx.scene.image.Image;
+import org.w3c.dom.css.RGBColor;
 
 
+/*
+    Nom : Cycle
+    Description : Classe gérant le cycle jour nuit en changeant le fond du canvas à interval régulier
+
+    -Attributs-
+    dayTime : s'incrémente à chaque passage dans la boucle de jeu
+    isNight : boolean pour savoir si c'est le jour ou la nuit
+    Image : sprite de la Lune.
+
+    -Méthode-
+    changerEtat : en fonction de la valeur de isNight, change le fond du canvas.
+
+ */
 public class Cycle {
     private int dayTime;
     private boolean isNight = false;
@@ -12,7 +26,7 @@ public class Cycle {
 
     public void changerEtat(Canvas canvas){
         dayTime++;
-        if(dayTime == 1500){
+        if(dayTime == 500){ //Durée normale 1500
             isNight = !isNight;
             if (isNight) dayTime = 300;
             else dayTime = 0;
@@ -20,10 +34,14 @@ public class Cycle {
         }
 
         if (isNight){
-            canvas.getGraphicsContext2D().setFill(Color.BLACK);
+            for(double luminosite = 1.0; luminosite > 0; luminosite = luminosite-0.00001 ){
+                System.out.println(luminosite);
+                canvas.getGraphicsContext2D().setFill((Color.hsb(122,1,luminosite)));
+
+            }
             canvas.getGraphicsContext2D().drawImage(moon, canvas.getWidth()/2-300, canvas.getHeight()/2-300, 80, 75);
         }
-        else canvas.getGraphicsContext2D().setFill(Color.LIGHTBLUE);
+        else canvas.getGraphicsContext2D().setFill(Color.hsb(200,1,1));
     }
 
 }
