6add0b45f74da6dd6f2b483f9b376978ce816f17
Ben <bengregorygbr@hotmail.com>
2022-01-07 18:25:04 +0100

    bruh ça marche

--- code/src/controller/Menu.java
+++ code/src/controller/Menu.java
@@ -3,6 +3,7 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -24,14 +25,28 @@ public class Menu {
     }
 
     public void handlePlay(ActionEvent actionEvent) {
+        try{
+
+
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/selectWorld.fxml"));
+            Scene scene = new Scene(root, 300, 275);
+            Stage stage = (Stage) buttonClose.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
 //        try {
 //            GameManager.getInstance().menuController.LoadFXML("/fxml/selectWorld.fxml");
 //        }
 //        catch (Exception e){
 //            e.printStackTrace();
 //        }
-        handleClose(actionEvent);
-        GameManager.getInstance().startGame(80,30);
+//        handleClose(actionEvent);
+//        GameManager.getInstance().startGame(80,30);
     }
 
 
