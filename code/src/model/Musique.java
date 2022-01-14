aed3c6fcac43ebc44f5b77c9bed5cb67c1f5109f
Ben <bengregorygbr@hotmail.com>
2022-01-14 16:50:02 +0100

    ajout musique dans le jeu

---
+++ code/src/model/Musique.java
@@ -0,0 +1,40 @@
+package model;
+
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+
+/**
+ * Permet de joueur une musique lors du lancement d'un monde
+ */
+public class Musique {
+    /**
+     * Thread dédié à la musique
+     */
+    private Thread musiqueThread;
+
+    /**
+     * Instancie le thread, récupère la musique et la lance
+     */
+    public void playSound(){
+
+            musiqueThread  = new Thread("musique"){
+                public void run(){
+                    String musicFile = "code/resources/music/sweden.wav";     // For example
+                    Media sound = new Media(new File(musicFile).toURI().toString());
+                    MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                    mediaPlayer.play();
+                }
+
+            };
+            musiqueThread.start();
+    }
+
+    /**
+     * Stop le thread de la musique
+     */
+    public void stopMusique(){
+        musiqueThread.stop();
+    }
+}
