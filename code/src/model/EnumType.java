4a1ee711a32d621605170e7bda2073ce13231acd
Ben <bengregorygbr@hotmail.com>
2022-01-14 16:52:12 +0100

    ajout de numéro correspondant en commentaire

--- code/src/model/EnumType.java
+++ code/src/model/EnumType.java
@@ -1,12 +1,12 @@
 package model;
 
 public enum EnumType {
-    Air,
-    Roche,
-    Terre,
-    Herbe,
-    Bedrock,
-    Tronc,
-    Feuilles,
-    Erreur
+    Air,        //0
+    Roche,      //1
+    Terre,      //2
+    Herbe,      //3
+    Bedrock,    //4
+    Tronc,      //5
+    Feuilles,   //6
+    Erreur      //7
 }
