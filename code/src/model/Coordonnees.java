f06e049e7eae6cda200606ada0c5bf0ebf098830
Ben <bengregorygbr@hotmail.com>
2022-01-18 17:12:06 +0100

    Ajout javadoc

--- code/src/model/Coordonnees.java
+++ code/src/model/Coordonnees.java
@@ -2,6 +2,9 @@ package model;
 
 import javafx.scene.canvas.Canvas;
 
+/**
+ * Classe gérant le passage de coordonnée du canvas en coordonnée dans le monde et vice-versa.
+ */
 public class Coordonnees {
     public double x;
     public double y;
@@ -13,6 +16,16 @@ public class Coordonnees {
         this.y = y;
     }
 
+    /**
+     * A partir d'une position dans le monde retourne la position qui correspond sur le canvas.
+     * @param xOnMonde position x dans le monde
+     * @param yOnMonde position y dans le monde
+     * @param xJoueur position x du joueur dans le monde
+     * @param yJoueur position y du joueur dans le monde
+     * @param canvas canvas en question
+     * @param cellSize taille d'un bloc
+     * @return Instance de Coordonnée avec les nouvelles valeurs
+     */
     public Coordonnees positionToCanvas(double xOnMonde, double yOnMonde, double xJoueur, double yJoueur, Canvas canvas, int cellSize){
         Coordonnees coo = new Coordonnees();
         coo.x = - xJoueur + canvas.getWidth()/2 + xOnMonde*cellSize;
@@ -20,6 +33,17 @@ public class Coordonnees {
         return coo;
     }
     //positionToCanvas en inversé, ici coo.x et coo.y sont les coordonnées sur la map
+
+    /**
+     * A partir d'une position dans le canvas retourne la position qui correspond dans le monde.
+     * @param xOnCanvas position x sur le canvas
+     * @param yOnCanvas position y sur le canvas
+     * @param xJoueur   position x du joueur sur le canvas
+     * @param yJoueur   position y du joueur sur le canvas
+     * @param canvas    le canvas en question
+     * @param cellSize  taille d'un bloc
+     * @return Instance de Coordonnée avec les nouvelles valeurs.
+     */
     public Coordonnees CanvasToPosition(double xOnCanvas, double yOnCanvas, double xJoueur, double yJoueur, Canvas canvas, int cellSize){
         Coordonnees coo = new Coordonnees();
         coo.x = (xOnCanvas + xJoueur - canvas.getWidth()/2)/cellSize;
