f27769e45d0899d712323f78484e51019727cbc8
Ben <bengregorygbr@hotmail.com>
2022-01-20 16:14:49 +0100

    javadoc

--- code/src/model/DeplacerJoueur.java
+++ code/src/model/DeplacerJoueur.java
@@ -8,14 +8,36 @@ import java.util.ArrayList;
  */
 public class DeplacerJoueur {
     private final Scene mainJeu;
+    /**
+     * Joueur à déplacer.
+     */
     private final Joueur joueur;
+    /**
+     * Booléen pour savoir si le bloc du dessus est vide (ce qui corresponde à un bloc d'air).
+     */
     public Boolean IsBlockUpEmpty;
+    /**
+     * Booléen pour savoir si le bloc du dessous est vide (ce qui corresponde à un bloc d'air).
+     */
     public Boolean IsBlockDownEmpty;
+    /**
+     * Booléen pour savoir si le bloc à droite est vide (ce qui corresponde à un bloc d'air).
+     */
     public Boolean IsBlockRightEmpty;
+    /**
+     * Booléen pour savoir si le bloc à gauche est vide (ce qui corresponde à un bloc d'air).
+     */
     public Boolean IsBlockLeftEmpty;
+    /**
+     * Instance de la classe Gravité.
+     */
     public Gravity gravity;
-
+    /**
+     * Tableau contenant les touches appuyées.
+     */
     public ArrayList<String> input;
+
+
     public DeplacerJoueur(Joueur joueur, Scene mainJeu){
         this.joueur = joueur;
         this.mainJeu = mainJeu;
@@ -24,6 +46,9 @@ public class DeplacerJoueur {
         init();
     }
 
+    /**
+     * Réupère la dernière touche appuyée et l'ajoute au tableau input.
+     */
     private void init(){
         mainJeu.setOnKeyPressed(
                 event -> {
@@ -39,6 +64,10 @@ public class DeplacerJoueur {
                     input.remove(code);
                 });
     }
+
+    /**
+     * En fonction des touches appuyées, déplace le joueur dans la bonne direction.
+     */
     public void deplacerJoueur(){
         if(input.contains("LEFT") || input.contains("Q")){
             joueur.isFacingRight.set(false);
@@ -82,6 +111,9 @@ public class DeplacerJoueur {
         joueur.y = joueur.y + joueur.ySpeed;
     }
 
+    /**
+     * Réduit la vitesse à 0 si il rentre en collision avec un joueur.
+     */
     private void checkCollisions(){
         if(!IsBlockDownEmpty){
             if (joueur.ySpeed > 0){
